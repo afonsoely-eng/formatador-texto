b61583d9d607f14f9fa9a7440079a3ddd2ae7b18
afonsoely-eng <afonso_ely@estudante.sc.senai.br>
2026-03-16 20:08:54 -0300

    feat: estrutura inicial do projeto

--- funcao-formatar.js
+++ funcao-formatar.js
@@ -1,5 +1,15 @@
 function formatarTexto(texto) {
-  // TODO: implementar
+  if (!texto) return "";
+
+  // remover espaços extras
+  texto = texto.trim().replace(/\s+/g, " ");
+
+  // deixar tudo minúsculo
+  texto = texto.toLowerCase();
+
+  // capitalizar início das frases
+  texto = texto.replace(/(^\w|\.\s+\w)/g, (letra) => letra.toUpperCase());
+
   return texto;
 }
 
--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const formatarTexto = require("./funcao-formatar");
 const textoFormatado = formatarTexto("análise e desenvolvimento de sistemas");
 
 console.log("Texto formatado:", textoFormatado);
+
+
